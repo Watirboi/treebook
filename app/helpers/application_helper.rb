af7b285dc8e6947691074957c294d2bdadd536d6
Watirboi <kwhit@hotmail.co>
2013-12-26 21:21:05 -0600

    Styled the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def flash_class(type)
+    case type
+      when :danger
+        "alert-error"
+      when :notice
+        "alert-success"
+      else
+        ""
+    end
+  end
 end
